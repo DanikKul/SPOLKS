e95d7cfe9cc00af7018a9b1fe28157174eefeaaa
danikkul <kulakovich2@gmail.com>
2024-11-13 18:30:28 +0300

    update L3

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,63 @@
-FROM python:alpine3.17
-WORKDIR /app
-COPY . /app
-RUN ls
-#RUN pip install -r requirements
-ENTRYPOINT ["python", "main.py"]
+FROM ubuntu:18.04
+
+RUN apt-get update && apt-get install -y openssh-server
+RUN mkdir /var/run/sshd
+
+RUN adduser --disabled-password --gecos "" worker
+RUN echo 'worker ALL=(ALL) NOPASSWD: ALL' >> /etc/sudoers
+
+RUN passwd -d worker
+
+ENV USER worker
+ENV HOME /home/$USER
+ENV MPI_DIR=/opt/ompi
+ENV PATH="$MPI_DIR/bin:$HOME/.local/bin:$PATH"
+ENV LD_LIBRARY_PATH="$MPI_DIR/lib:$LD_LIBRARY_PATH"
+WORKDIR $HOME
+
+RUN sed 's@session\s*required\s*pam_loginuid.so@session optional pam_loginuid.so@g' -i /etc/pam.d/sshd
+
+RUN apt-get -q update \
+    && apt-get install -y \
+    python3 python3-dev python3-pip \
+    gcc gfortran binutils \
+    && pip3 install --upgrade pip \
+    && apt-get clean \
+    && rm -rf /var/lib/apt/lists/* /tmp/* /var/tmp/*
+
+ADD https://download.open-mpi.org/release/open-mpi/v5.0/openmpi-5.0.5.tar.bz2 .
+RUN tar xf openmpi-5.0.5.tar.bz2 \
+    && cd openmpi-5.0.5 \
+    && ./configure \
+    && make -j4 all \
+    && make install \
+    && cd .. && rm -rf \
+    openmpi-5.0.5 openmpi-5.0.5.tar.bz2 /tmp/*
+
+RUN  apt-get update \
+  && apt-get install -y git
+
+RUN apt install zlib1g-dev
+
+ENV NOTVISIBLE "in users profile"
+RUN echo "export VISIBLE=now" >> /etc/profile
+
+RUN pip3 install setuptools
+RUN pip3 install mpi4py
+RUN pip3 install numpy
+
+RUN sed -i 's/#PermitRootLogin prohibit-password/PermitRootLogin yes/' /etc/ssh/sshd_config
+RUN sed -i 's/#PermitEmptyPasswords no/PermitEmptyPasswords yes/' /etc/ssh/sshd_config
+RUN sed -i 's/#PermitEmptyPasswords yes/PermitEmptyPasswords yes/' /etc/ssh/sshd_config
+RUN sed -i 's/#UsePAM yes/UsePAM no/' /etc/ssh/sshd_config
+RUN sed -i 's/UsePAM yes/UsePAM no/' /etc/ssh/sshd_config
+RUN sed -i 's/#UsePAM no/UsePAM no/' /etc/ssh/sshd_config
+
+USER $USER
+
+RUN git clone https://github.com/DanikKul/SPOLKS.git
+
+USER root
+
+EXPOSE 22
+CMD ["/usr/sbin/sshd", "-D"]
--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import mpi4py
 from mpi4py import MPI
 import numpy as np
 
@@ -14,13 +15,19 @@ def matrix_multiply(A, B):
 comm = MPI.COMM_WORLD
 rank = comm.Get_rank()
 size = comm.Get_size()
-shape = 10
-
+shape = 500
 print(f"Process {rank} on processor {MPI.Get_processor_name()}")
 
 if rank == 0:
     A = np.random.randint(0, 10, (shape, shape))
     B = np.random.randint(0, 10, (shape, shape))
+
+    t1 = MPI.Wtime()
+    C = matrix_multiply(A, B)
+    t2 = MPI.Wtime()
+    print(f"One-thread: {t2 - t1} sec")
+
+    t1 = MPI.Wtime()
     chunk_size = A.shape[0] // size
     A_chunks = [A[i:i + chunk_size] for i in range(0, A.shape[0], chunk_size)]
     for i in range(1, size):
@@ -34,10 +41,66 @@ if rank == 0:
         for i in range(len(A_chunks) % size):
             C_pp = matrix_multiply(A_chunks[i + size], B)
             C_partial = np.vstack((C_partial, C_pp))
-    print(C_partial)
-    print(np.matmul(A, B))
+    t2 = MPI.Wtime()
+    print(f"Blocking parallel: {t2 - t1} sec")
+
+    comm.Barrier()
+
+    t1 = MPI.Wtime()
+    chunk_size = A.shape[0] // size
+    A_chunks = [A[i:i + chunk_size] for i in range(0, A.shape[0], chunk_size)]
+    reqsA = []
+    reqsB = []
+    for i in range(1, size):
+        reqsA.append(comm.isend(A_chunks[i], dest=i, tag=1))
+        reqsB.append(comm.isend(B, dest=i, tag=2))
+    C_partial = matrix_multiply(A_chunks[0], B)
+    bufsize = 1 << 30
+    for i in range(len(reqsA)):
+        reqsA[i].wait()
+        reqsB[i].wait()
+    reqsC = []
+    for i in range(1, size):
+        C_pp = comm.irecv(bufsize, source=i, tag=3).wait()
+        C_partial = np.vstack((C_partial, C_pp))
+    if A.shape[0] % size != 0:
+        for i in range(len(A_chunks) % size):
+            C_pp = matrix_multiply(A_chunks[i + size], B)
+            C_partial = np.vstack((C_partial, C_pp))
+    t2 = MPI.Wtime()
+    print(f"Non-blocking parallel: {t2 - t1} sec")
 else:
     A_chunk = comm.recv(source=0, tag=1)
     B = comm.recv(source=0, tag=2)
     C_partial = matrix_multiply(A_chunk, B)
     comm.send(C_partial, dest=0, tag=3)
+
+    comm.Barrier()
+
+    bufsize = 1 << 30
+    req_A = comm.irecv(bufsize, source=0, tag=1)
+    B_req = comm.irecv(bufsize, source=0, tag=2)
+    A_chunk = req_A.wait()
+    B = B_req.wait()
+    C_partial = matrix_multiply(A_chunk, B)
+    comm.isend(C_partial, dest=0, tag=3).wait()
+
+# import numpy
+# from mpi4py import MPI
+#
+# comm = MPI.COMM_WORLD
+# rank = comm.Get_rank()
+#
+# if rank == 0:
+#     data = numpy.ones(5000)
+#     print(data)
+#     req = comm.isend(data, dest=1, tag=11)
+#     req.wait()
+# elif rank == 1:
+#     req = comm.irecv(source=0, tag=11)
+#     # data = req.wait()
+#     # print(data)
+#     success, data = req.test()
+#     while not success:
+#         success, data = req.test()
+#         print(rank, data)
